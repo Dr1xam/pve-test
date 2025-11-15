0146dc2d9bdf1d9f334c594458f037a6c2f686bb
Dr1xam <Dr1xam>
2025-11-15 11:34:19 +0200

    ще одна спроба запускатися не з труби

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ ENV_URL="https://raw.githubusercontent.com/Dr1xam/pve-test/refs/heads/main/.env"
 
 
 curl -sL "$ENV_URL" -o .env
-nano .env
+nano .env answer < /dev/tty
 
 source .env
 echo "Значення змінної:"
