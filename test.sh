9479fb4032d1dd8c98f040b44f367152fae5be0f
Dr1xam <Dr1xam>
2025-11-15 10:32:04 +0200

    + новий тест із силкою на env файл

--- test.sh
+++ test.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+
+ENV_URL="https://raw.githubusercontent.com/Dr1xam/pve-test/refs/heads/main/.env"
+
+
+curl -sL "$ENV_URL" -o .env
+
 source .env
 echo "Значення змінної:"
 echo "$MY_VARIABLE"
+
+rm .env
+rm test.sh
